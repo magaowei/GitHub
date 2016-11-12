a162b03e6e5f3b89741519cf85f4d53bff951546
magaowei <752686342@qq.com>
2016-11-12 18:03:41 +0800

    增加界面

---
+++ app/src/main/java/wuxianedu/com/github/OtherActivity.java
@@ -0,0 +1,13 @@
+package wuxianedu.com.github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OtherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+    }
+}
